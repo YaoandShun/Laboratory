a14b836bfef3646936b8fdf5254aefb85d94dd7d
YaoandShun <842977672@qq.com>
2021-04-15 22:46:42 +0800

    feat: learn some new chapter content

---
+++ LearnAppleDevelop/day02/Test.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let someConstant: Int
+
+
+
+
+
+
+var someVariable:Int
+someVariable = 1
+print(someVariable)
---
+++ LearnAppleDevelop/day02/readme.txt
@@ -0,0 +1,3 @@
+Book:【Swift与Cocoa框架开发】
+
+1、section comes to 2.4

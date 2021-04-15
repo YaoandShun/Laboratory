8a326a8391167b53179dbbfdcfe6c22705c3d277
YaoandShun <842977672@qq.com>
2021-04-15 18:59:24 +0800

    bug: cant fix demo

--- LearnAppleDevelop/day01_HelloWorld/iOSPlatform/HelloCocoa/HelloCocoa/ViewController.swift
+++ LearnAppleDevelop/day01_HelloWorld/iOSPlatform/HelloCocoa/HelloCocoa/ViewController.swift
@@ -10,25 +10,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var helloButton: UIButton!
+//    @IBAction func showAlert(_ sender: Any) {
+////        var alert = UIAlertController(title: "Hello!", message: "Hello, world!",preferredStyle: UIAlertControllerStyle.Alert)
+//        let alert = UIAlertController(title: "Hello", message: "Hello, world", preferredStyle: UIAlertController.Style.alert)
+//
+//        alert.addAction(UIAlertAction(title: "close", style: UIAlertAction.Style.default, handler: nil))
+//
+//        self.present(alert,animated:true,completion:nil)
+////        ❌ it didn't work
+////        test
+//        self.helloButton.setTitle("Clicked",for:.normal)
+//
+//
+//    }
+//
     
-    @IBAction func showAlert(_ sender: Any) {
-//        var alert = UIAlertController(title: "Hello!", message: "Hello, world!",preferredStyle: UIAlertControllerStyle.Alert)
-        
+    @IBAction func helloButtonTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Hello", message: "Hello, world", preferredStyle: UIAlertController.Style.alert)
-        
-        alert.addAction(UIAlertAction(title: "close", style: UIAlertAction.Style.default, handler: nil))
-        
-        self.present(alert,animated:true,completion:nil)
-//        ❌ it didn't work
-//        test
-        self.helloButton.setTitle("Clicked",for:.normal)
-        
-        
-            
-    }
-    
-    
-    
+        alert.addAction(UIAlertAction(title: "close", style: .default, handler: { (_) in
+            self.helloButton.setTitle("Clicked",for:.normal)
+            self.helloButton.setTitle("Clicked",for:.normal)
+        }))
+        self.present(alert,animated:true,completion:nil)}
     
     
     override func viewDidLoad() {

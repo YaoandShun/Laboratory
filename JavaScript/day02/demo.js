6bc42aea4d039bc1040c00bcfca86b990b1e030b
YaoandShun <842977672@qq.com>
2021-04-26 21:00:55 +0800

    fix: a day a little

--- JavaScript/day02/demo.js
+++ JavaScript/day02/demo.js
@@ -9,3 +9,13 @@ function createParagraph() {
   for(let i = 0; i < buttons.length ; i++) {
     buttons[i].addEventListener('click', createParagraph);
   } 
+/*
+1. 创建功能函数
+    1. 创建元素
+    2. 指定元素内容
+    3. 添加到 dom 对象中去
+2. 找出所有button对象
+    1. 遍历 button 对象
+        1. 往 button 对象中绑定功能函数
+
+*/
